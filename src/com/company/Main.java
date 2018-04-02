0993056ada31f46e23488aa22196eab7395b7860
Avery Snyder <madrauk49@gmail.com>
2018-04-02 12:44:24 -0400

    Part 1 finished.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.rmi.Naming;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        try{
+            int RMIPort;
+            String hostName;
+            InputStreamReader is = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(is);
+            System.out.println("Enter the RMIRegistry host name:");
+            hostName = br.readLine();
+            System.out.println("Enter the RMIRegistry port number:");
+            String portNum = br.readLine();
+            RMIPort = Integer.parseInt(portNum);
+            String registryURL = "rmi://" + hostName + ":" + portNum + "/int";
+            IntInterface h = (IntInterface)Naming.lookup(registryURL);
+            System.out.println("Lookup completed " );
+            int message = h.addInt(5,17);
+            System.out.println("IntClient: " + message);
+        }
+        catch (Exception e){
+            System.out.println("Exception in IntClient: " + e);
+        }
     }
 }
